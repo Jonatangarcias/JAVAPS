59d8db60b1c652599b9582282433aa7ccabd610b
Jonatan Alexander Garcia Sepulveda <jonatangarcia2346gmail.com>
2023-07-19 21:24:33 -0500

    Actualizacion ejecicios de clase

---
+++ src/main/java/co/com/ps/c11/SaludosEnum.java
@@ -0,0 +1,11 @@
+package co.com.ps.C11;
+
+public enum SaludosEnum {
+    WELLCOME("Bienvenido al area de estados");
+
+    private final String descripcion;
+
+    SaludosEnum(String descripcion){
+        this.descripcion=descripcion;
+    }
+}
--- src/main/java/co/com/ps/c7/Ejercicio.java
+++ src/main/java/co/com/ps/c7/Ejercicio.java
@@ -2,18 +2,32 @@ package co.com.ps.c7;
 
 //Dado un arreglo {4,2,6,2,3,5} ordenar de menor a mayor
 
+import java.util.Arrays;
+
 public class Ejercicio {
 
     public static void main(String[] args) {
 
-        int [] numeros =new int [6];
-        numeros [0]= 4;
-        numeros [1]= 2;
-        numeros [2]= 6;
-        numeros [3]= 2;
-        numeros [4]= 3;
-        numeros [5]= 5;
+        int[] numeros = new int[6];
+        numeros[0] = 4;
+        numeros[1] = 2;
+        numeros[2] = 6;
+        numeros[3] = 2;
+        numeros[4] = 3;
+        numeros[5] = 5;
 
+        int vari = 0;
+
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 1; j < (numeros.length - i); j++) {
+                if (numeros[j - 1] > numeros[j]) {
+                    vari = numeros[j - 1];
+                    numeros[j - 1] = numeros[j];
+                    numeros[j] = vari;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(numeros));
 
     }
 }
